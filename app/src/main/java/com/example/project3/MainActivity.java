393218f05af022a13a4f17be689b64af82375885
Jamie Mortensen <16mortensenj@gmail.com>
2022-08-29 22:35:34 -0400

    added some variables to main activity_main.xml

--- app/src/main/java/com/example/project3/MainActivity.java
+++ app/src/main/java/com/example/project3/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.project3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText mNameEditText;
+    EditText mPasswordEditText;
+    Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View v){
+
     }
 }
