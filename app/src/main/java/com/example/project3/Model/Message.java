094a0d4066b23cb72c447e564f20f5ee972a1da5
Jamie Mortensen <16mortensenj@gmail.com>
2022-08-30 20:57:13 -0400

    implemented Message

--- app/src/main/java/com/example/project3/Model/Message.java
+++ app/src/main/java/com/example/project3/Model/Message.java
@@ -2,4 +2,40 @@ package com.example.project3.Model;
 
 public class Message {
 
+    private String mMessageBody;
+    private int mSenderId;
+    private int mParentMessageId;
+    private int mRecipientId;
+    public Message(int id, String messageBody,int senderId,int parentMessageId,int RecipientId){
+        mId = id;
+        mMessageBody = messageBody;
+        mSenderId = senderId;
+        mParentMessageId = parentMessageId;
+        mRecipientId = RecipientId;
+
+    }
+    public int getId() {
+        return mId;
+    }
+
+    private int mId;
+
+    public String getMessageBody() {
+        return mMessageBody;
+    }
+
+    public int getSenderId() {
+        return mSenderId;
+    }
+
+    public int getParentMessageId() {
+        return mParentMessageId;
+    }
+
+    public int getRecipientId() {
+        return mRecipientId;
+    }
+
+
+
 }
