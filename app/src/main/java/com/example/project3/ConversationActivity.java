ec2f959ce8f70b32a0e12991514c428cf6b25b05
Jamie Mortensen <16mortensenj@gmail.com>
2022-08-31 15:07:01 -0400

    Duplicated RefreshChats into COnversation

--- app/src/main/java/com/example/project3/ConversationActivity.java
+++ app/src/main/java/com/example/project3/ConversationActivity.java
@@ -1,13 +1,30 @@
 package com.example.project3;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.example.project3.Model.Conversations;
+import com.example.project3.Model.Message;
 import com.example.project3.Model.Model;
+import com.example.project3.Model.User;
+import com.example.project3.Server.ServerInterface;
+
+import org.json.JSONException;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class ConversationActivity extends AppCompatActivity {
     private RecyclerView mMessageRecycler;
@@ -24,5 +41,113 @@ public class ConversationActivity extends AppCompatActivity {
         mMessageAdapter = new ConversationAdapter(this, conv);
         mMessageRecycler.setLayoutManager(new LinearLayoutManager(this));
         mMessageRecycler.setAdapter(mMessageAdapter);
+    }
+
+
+    final class RefreshChats extends AsyncTask<Void, Integer, Conversations> {
+        private final WeakReference<Activity> parentRef;
+        private final WeakReference<ListView> listViewRef;
+
+
+        public RefreshChats(final Activity parent, ListView listView){
+            parentRef = new WeakReference<Activity>(parent);
+            listViewRef = new WeakReference<ListView>(listView);
+        }
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        @Override
+        protected Conversations doInBackground(Void... voids) {
+            try {
+                ArrayList<Message> sentMessages = ServerInterface.Messages.getSentMessages(Model.getInstance().getUser().id);
+                ArrayList<Message> receivedMessages = ServerInterface.Messages.getRecievedMessages(Model.getInstance().getUser().id);
+
+//                messages.addAll(sentMessages);
+//                messages.addAll(receivedMessages);
+
+
+
+                // TODO Build conversations and save into model
+                ArrayList<Integer> conversationIds = new ArrayList();
+                Conversations conversations = new Conversations();
+                for (int i=0; i<sentMessages.size();i++){
+                    int messageConversationId = sentMessages.get(i).getConversationId();
+                    if (!conversationIds.contains(messageConversationId)){
+                        conversationIds.add(messageConversationId);
+                    }
+                }
+                for (int i=0; i<receivedMessages.size();i++){
+                    int messageConversationId = sentMessages.get(i).getConversationId();
+                    if (!conversationIds.contains(messageConversationId)){
+                        conversationIds.add(messageConversationId);
+                    }
+                }
+
+                int otherUserId =0;
+                for (int i=0; i<conversationIds.size(); i++){
+//                    ArrayList<Message> convSent = ServerInterface.Messages.getSentMessages(Model.getInstance().getUser().id);
+//                    ArrayList<Message> convReceived = ServerInterface.Messages.getRecievedMessages(Model.getInstance().getUser().id);
+
+                    ArrayList<Message> messages = new ArrayList<>();
+
+                    for (int j=0; j<receivedMessages.size();j++){
+                        int messageConversationId = receivedMessages.get(i).getConversationId();
+                        if (conversationIds.get(i) == messageConversationId){
+                            otherUserId = receivedMessages.get(i).getSenderId();
+                            messages.add(receivedMessages.get(i) );
+                        }
+                    }
+
+                    for (int j=0; j<sentMessages.size();j++){
+                        int messageConversationId = sentMessages.get(i).getConversationId();
+                        if (conversationIds.get(i) == messageConversationId){
+                            otherUserId = sentMessages.get(i).getRecipientId();
+                            messages.add(sentMessages.get(i));
+                        }
+                    }
+                    if (messages.size()>0){
+                        if (otherUserId != 0) {
+                            User other = ServerInterface.Users.getUserById(otherUserId);
+                            messages.sort(Comparator.comparing(Message::getParentMessageId));
+                            Conversations.Conversation conv = new Conversations.Conversation(messages,other,conversationIds.get(i));
+                            conversations.addConversation(conv);
+                        }
+                    }
+
+                }
+
+
+                return conversations;
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        }
+        @Override
+        protected void onPostExecute(Conversations result) {
+
+            Model.getInstance().setConversations(result);
+
+            Model.getInstance().getConversations();
+            listViewRef.get();
+
+            ;
+            ChatsList adapter = new ChatsList(ChatsActivity.this, Model.getInstance().getConversations().getChatNames());
+
+
+            ListView listView = (ListView) findViewById(R.id.my_list_view);
+            listView.setAdapter(adapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
+
+
+                    Intent intent = new Intent(parentRef.get().getApplicationContext(), ConversationActivity.class);
+                    startActivity(intent);
+
+                }
+            });
+        }
     }
 }
